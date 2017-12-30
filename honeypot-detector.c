0d68289f6f1245841eeadf36160df4027169fc1a
Jorge Matricali <jorgematricali@gmail.com>
2017-12-30 14:34:12 -0300

    + print_debug + print_error

--- honeypot-detector.c
+++ honeypot-detector.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,7 +9,32 @@
 
 #define BUF_SIZE 1024
 
-int probe(char *serverAddr, unsigned int port)
+int g_verbose = 0;
+
+void print_error(const char *format, ...)
+{
+    va_list arg;
+    fprintf(stderr, "\t\033[91m[!] ");
+    va_start(arg, format);
+    vfprintf(stderr, format, arg);
+    va_end (arg);
+    fprintf(stderr, "\033[0m\n");
+}
+
+void print_debug(const char *format, ...)
+{
+    if (g_verbose != 1) {
+        return;
+    }
+    va_list arg;
+    fprintf(stderr, "\033[37m");
+    va_start(arg, format);
+    vfprintf(stderr, format, arg);
+    va_end (arg);
+    fprintf(stderr, "\033[0m\n");
+}
+
+int probe(char *serverAddr, unsigned int serverPort)
 {
     struct sockaddr_in addr;
     int sockfd, ret;
@@ -16,10 +42,11 @@ int probe(char *serverAddr, unsigned int port)
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        printf("Error creating socket!\n");
-        exit(1);
+        print_error("Error creating socket!\n");
+        sockfd = 0;
+        return -1;
     }
-    // printf("Socket created...\n");
+    print_debug("Socket created.\n");
 
     struct timeval timeout;
     timeout.tv_sec = 10;
@@ -32,87 +59,80 @@ int probe(char *serverAddr, unsigned int port)
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(serverAddr);
-    addr.sin_port = htons(port);
+    addr.sin_port = htons(serverPort);
 
+    print_debug("\t[-] %s:%d Connecting...\n", serverAddr, serverPort);
     ret = connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));
     if (ret < 0) {
-        printf("\t[!] %s - Error connecting to the server!\n", serverAddr);
+        print_error("%s:%d - Error connecting to the server!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
     }
-    // printf("Connected to the server...\n");
+    print_debug("\t[!] %s:%d Connected.\n", serverAddr, serverPort);
 
     memset(buffer, 0, BUF_SIZE);
 
     // RECIBIR BANNER
     ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
     if (ret < 0) {
-        printf("\t[!] %s - Error receiving banner!\n", serverAddr);
+        print_error("%s:%d - Error receiving banner!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
-    } //else {
-        // printf("BANNER:\n%s\n", buffer);
-    // }
+    }
+    print_debug("\t[-] %s:%d - BANNER: %s\n", serverAddr, serverPort, buffer);
 
     char *pkt1 = "SSH-2.0-OpenSSH_7.5";
     char *pkt2 = "\n";
     char *pkt3 = "asd\n      ";
     char *search = "Protocol mismatch.";
 
-    // printf("<< ENVIANDO pkt1\n");
+    print_debug("\t[<] %s:%d - Sending pkt1: %s\n", serverAddr, serverPort, pkt1);
     ret = sendto(sockfd, pkt1, sizeof(pkt1), 0, (struct sockaddr *) &addr, sizeof(addr));
-    // printf("-- ENVIADO!\n");
+
     if (ret < 0) {
-        printf("\t[!] %s - Error sending data pkt1!!\n", serverAddr);
+        print_error("%s:%d - Error sending data pkt1!!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
-    } else {
-        // printf(">>PKT1\n");
     }
 
-    // printf("<< ENVIANDO pkt2\n");
+    print_debug("\t[<] %s:%d - Sending pkt2: %s\n", serverAddr, serverPort, pkt2);
     ret = sendto(sockfd, pkt2, sizeof(pkt2), 0, (struct sockaddr *) &addr, sizeof(addr));
-    // printf("-- ENVIADO!\n");
+
     if (ret < 0) {
-        printf("\t[!] %s - Error sending data pkt2!!\n", serverAddr);
+        print_error("%s:%d - Error sending data pkt2!!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
-    } else {
-        // printf(">>PKT2\n");
     }
 
-    // printf("<< ENVIANDO pkt3\n");
+    print_debug("\t[<] %s:%d - Sending pkt3: %s\n", serverAddr, serverPort, pkt3);
     ret = sendto(sockfd, pkt3, sizeof(pkt3), 0, (struct sockaddr *) &addr, sizeof(addr));
-    // printf("-- ENVIADO!\n");
+
     if (ret < 0) {
-        printf("\t[!] %s - Error sending data pkt3!!\n", serverAddr);
+        print_error("%s:%d - Error sending data pkt3!!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
-    } else {
-        // printf(">>PKT3\n");
     }
 
-    // printf(">> RECIBIENDO...\n");
+    print_debug("\t[>] %s:%d - Receiving...\n", serverAddr, serverPort);
     ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
-    // printf("|| RECIBIDO!\n");
     if (ret < 0) {
-        printf("\t[!] %s - Error receiving response!!\n", serverAddr);
+        print_error("%s:%d - Error receiving response!!\n", serverAddr, serverPort);
         close(sockfd);
         sockfd = 0;
         return -1;
-    } else {
-        // printf("RET:\n%s\n", buffer);
     }
+    print_debug("\t[+] %s:%d - Received: %s\n", serverAddr, serverPort, buffer);
+
 
     if (strstr(buffer, search) != NULL) {
-        printf("\t[+] %s - OPEN SSH LEGITIMO\n", serverAddr);
+        printf("\t[+] %s:%d - OK\n", serverAddr, serverPort);
     } else {
-        printf("\t[!] %s - POSIBLE HONEYPOT!\n", serverAddr);
+        printf("\t[!] %s:%d - POSSIBLE HONEYPOT!\n", serverAddr, serverPort);
     }
 
     close(sockfd);
